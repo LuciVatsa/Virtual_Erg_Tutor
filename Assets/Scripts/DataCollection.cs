92de7f31fb0c056489f0d8b373cc0b969728a03a
LuciVatsa <saaranshvatsa@yahoo.in>
2020-03-09 17:09:16 -0600

    Data Collection

---
+++ Assets/Scripts/DataCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class DataCollection : MonoBehaviour
+{   void Update()
+    {
+         using (StreamWriter sw = new StreamWriter("Assets/CurrentInfo.csv", append:true))
+        {
+            sw.WriteLine(name + "," + Time.time + "," 
+            + gameObject.transform.position.x + "," + gameObject.transform.position.y 
+            + "," + gameObject.transform.position.z + "," + gameObject.transform.rotation.eulerAngles.x 
+            + "," + gameObject.transform.rotation.eulerAngles.y + "," + gameObject.transform.rotation.eulerAngles.z);
+        }
+    }
+}
---
+++ Assets/Scripts/DataCollectionTxT.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEngine;
+
+public class DataCollectionTxT : MonoBehaviour
+{
+    string object_name;
+    // Start is called before the first frame update
+    void Start()
+    {
+        object_name = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        using (StreamWriter sw = new StreamWriter("Assets/CurrentInfo"+ object_name +".txt", append:true))
+        {
+            sw.WriteLine(object_name + "  " + Time.time + "  " 
+            + gameObject.transform.position.x + "  " + gameObject.transform.position.y 
+            + "  " + gameObject.transform.position.z + "  " + gameObject.transform.rotation.eulerAngles.x 
+            + "  " + gameObject.transform.rotation.eulerAngles.y + "  " + gameObject.transform.rotation.eulerAngles.z + "  ");
+        }
+    }
+}
